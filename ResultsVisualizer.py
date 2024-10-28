0aea7c64feadd7f45450b4f494792059553ba95a
salgabda <salgabda@naver.com>
2024-10-28 12:29:21 +0900

    feat: Resuntvisalizer

--- ResultsVisualizer.py
+++ ResultsVisualizer.py
@@ -1,13 +1,17 @@
 # 시뮬레이션 결과를 시각적으로 표현합니다.
-
 import matplotlib.pyplot as plt
 
 class ResultsVisualizer:
     def __init__(self, stay_rate, switch_rate):
-        self.stay_rate = stay_rate # 결정을 안 바꿨을을 때 비율
-        self.switch_rate = switch_rate # 결정을 바꿨을 때 비율
+        self.stay_rate = stay_rate
+        self.switch_rate = switch_rate
 
-        def plot_results(self):
-        # label를 만들어서 승률을 백분률로 변환시켜 plt.show()로 표현하는 기능
+    def plot_results(self):
+        labels = ['원래 선택 유지', '선택 번복']
+        rates = [self.stay_rate * 100, self.switch_rate * 100]
 
-
+        plt.bar(labels, rates, color=['blue', 'orange'])
+        plt.ylabel('승률 (%)')
+        plt.title('Monty Hall 문제 승률 비교')
+        plt.ylim(0, 100)
+        plt.show()
