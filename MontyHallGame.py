ab9e166db5e13cd2cadfb548fe3499cf7b5d6e52
salgabda <salgabda@naver.com>
2024-10-28 12:01:48 +0900

    feat: :wq Create class

--- MontyHallGame.py
+++ MontyHallGame.py
@@ -5,15 +5,15 @@ import random
 
 class MontyHallGame:
     def __init__(self):
-        self.doors = [0,0,0] # 0: 염소, 1 자동차
+        self.doors = [0, 0, 0]  # 0: goat, 1: car
         self.winner = None
-
     def setup_game(self):
-        # 자동차를 무작위로 배치합니다
-
+        # 자동차를 무작위로 배치
+        car_position = random.randint(0, 2)
+        self.doors[car_position] = 1  # 자동차 위치
+        self.winner = car_position
     def make_choice(self, player_choice):
-        # 플레이어의 선택을 받고, 열릴문을 결정합니다.
-
-
-
-
+        # 플레이어의 선택을 받고, 열릴 문을 결정
+        remaining_doors = [i for i in range(3) if i != player_choice and i != self.winner]
+        revealed_door = random.choice(remaining_doors)
+        return revealed_door
